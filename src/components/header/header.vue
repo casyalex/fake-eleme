7587bf7d7440f3fee456ff686e1f4e2ab8c3e7c9
alex <superalex_1104@163.com>
2018-10-05 17:33:10 +0800

    header样式 part6

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -26,6 +26,9 @@
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -43,8 +46,10 @@ export default {
 <style lang="stylus" scoped>
 @import "~@/common/stylus/mixin.styl"
 .header
+  position relative
   color #ffffff
-  background #999
+  background rgba(7,17,27,0.5)
+  overflow hidden
   .content-wrapper
     position relative
     padding 24px 12px 18px 24px
@@ -144,4 +149,12 @@ export default {
       font-size 10px
       right 12px
       top 8px
+  .background
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index -1
+    filter blur(10px)
 </style>
