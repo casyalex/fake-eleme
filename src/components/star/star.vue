c396aefc191ed6e1d25a4e58107984fa0ea6139b
alex <superalex_1104@163.com>
2018-10-07 01:00:39 +0800

    星星组件

---
+++ src/components/star/star.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="(item, index) in itemClasses" :key="index" class="star-item" :class="itemClass"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number
+    },
+    score: {
+      type: Number
+    }
+  },
+  computed: {
+    starType() {
+      return 'star-' + this.size
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~@/common/stylus/mixin.styl";
+.star
+  font-size 0
+  .star-item
+    display inline-block
+    background-repeat no-repeat
+  &.star-48
+    .star-item
+      width 20px
+      height 20px
+      margin-right 22px
+      background-size 20px 20px
+      &.on
+        bg-image('star48_on')
+      &.half
+        bg-image('star48_half')
+      &.off
+        bg-image('star48_off')
+
+  &.star-36
+  &.star-24
+
+</style>
+
+
