9c132da28bec327bae3e05ea7b71eee34ea58b02
Alex <superalex_1104@163.com>
2018-11-05 17:52:10 +0800

    加减按钮cartcontrolP3

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="cartcontorl">
+    <transition name="move">
+      <div class="cart-minus icon-remove_circle_outline" v-show="food.count > 0" @click="minusCart"></div>
+    </transition>
+    <div class="cart-count" v-show="food.count > 0">{{food.count}}</div>
+    <div class="cart-add icon-add_circle" @click="addCart"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  },
+  created () {
+    // console.log(this.food)
+  },
+  methods: {
+    addCart () {
+      if (!this.food.count) {
+        this.$set(this.food, 'count', 1)
+      } else {
+        this.food.count++
+      }
+    },
+    minusCart () {
+      if (this.food.count) {
+        this.food.count--
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .cartcontorl
+    font-size 0
+    .cart-minus,.cart-add
+      display inline-block
+      padding 6px
+      line-height 24px
+      font-size 24px
+      color rgb(0,160,220)
+    .cart-count
+      display inline-block
+      vertical-align top
+      width 12px
+      padding-top 6px
+      line-height 24px
+      text-align center
+      font-size 10px
+      color rgb(147,153,159)
+    .cart-add
+      display inline-block
+</style>
