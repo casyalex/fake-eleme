e14eb834dd7c545b035e3ecf6aae070e148609e1
Alex <superalex_1104@163.com>
2018-11-29 19:18:00 +0800

    '8-1'

---
+++ src/components/food/food.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-show="showFlag" class="food">
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
+
+
