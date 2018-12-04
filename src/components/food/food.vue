d68c4a7b1a7a180815330d3233f67c8c6c68e688
Alex <superalex_1104@163.com>
2018-12-04 16:33:48 +0800

    '8-3'

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -4,6 +4,19 @@
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image">
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -25,6 +38,9 @@ export default {
   methods: {
     show () {
       this.showFlag = true
+    },
+    hide () {
+      this.showFlag = false
     }
   }
 }
@@ -39,9 +55,35 @@ export default {
     z-index 30
     width 100%
     background #fff
-    transition all 0.2s linear
-    &.move-enter,&.move-leave {
+    transition all .3s linear
+    opacity 1
+    &.move-enter,&.move-leave-to {
+      opacity 0
       transform translate3d(100%, 0, 0)
+    }
+    .image-header {
+      position relative
+      width 100%
+      height 0
+      padding-top 100%
+      img {
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
+      }
+      .back {
+        position absolute
+        top 10px
+        left 0
+        .icon-arrow_lift {
+          display block
+          padding 10px
+          font-size 20px
+          color #fff
+        }
+      }
     }
   }
 </style>
